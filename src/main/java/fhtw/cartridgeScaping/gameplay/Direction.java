999e5ad44f0b5a266beb730e6d65253d6277f163
Nikolaus Rieder <ic20b028@technikum-wien.at>
2022-01-15 21:19:51 +0100

    Implemented Direction & Player class

--- src/main/java/fhtw/cartridgeScaping/gameplay/Direction.java
+++ src/main/java/fhtw/cartridgeScaping/gameplay/Direction.java
@@ -1,25 +1,28 @@
-package fhtw.cartridgeScaping.game;
+package fhtw.cartridgeScaping.gameplay;
 
 public enum Direction {
-    NORTH(),
-    EAST(),
-    SOUTH(),
-    WEST(),
-    IN(),
-    OUT();
+    NORTH(0, 1, "north", "You can go north."),
+    EAST(1, 0, "east", "You can go east."),
+    SOUTH(0, -1, "south", "You can go south."),
+    WEST(-1, 0, "west", "You can go west.");
 
     private int dx;
     private int dy;
-    private Door door;
+    private String dirName;
+    private String dirDesc;
 
-    public Direction() {}
-
-    public Direction(Door door) {
-        this.door = door;
-    }
-
-    public Direction(int dx, int dy) {
+    Direction(int dx, int dy, String dirName, String dirDesc) {
         this.dx = dx;
         this.dy = dy;
+        this.dirName = dirName;
+        this.dirDesc = dirDesc;
+    }
+
+    public String getDirName() {
+        return dirName;
+    }
+
+    public String getDirDesc() {
+        return dirDesc;
     }
 }
--- src/main/java/fhtw/cartridgeScaping/gameplay/Player.java
+++ src/main/java/fhtw/cartridgeScaping/gameplay/Player.java
@@ -1,2 +1,34 @@
-package fhtw.cartridgeScaping.gameplay;public class Player {
+package fhtw.cartridgeScaping.gameplay;
+
+import java.util.HashMap;
+
+public class Player {
+    private String playerName;
+    private HashMap<Integer, Item> inventory;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+        this.inventory = new HashMap<>();
+    }
+
+    public Player(String playerName, HashMap<Integer, Item> inventory) {
+        this.playerName = playerName;
+        this.inventory = inventory;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public HashMap<Integer, Item> getInventory() {
+        return inventory;
+    }
+
+    public void addItem(Item item) {
+        inventory.put(item.getId(), item);
+    }
 }
