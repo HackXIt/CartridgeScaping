f5f1203731d0662e688f2f0ea2bb55259a53a28a
Nikolaus Rieder <ic20b028@technikum-wien.at>
2022-01-15 02:48:15 +0100

    Moved View.java to util package

--- src/main/java/fhtw/cartridgeScaping/util/View.java
+++ src/main/java/fhtw/cartridgeScaping/util/View.java
@@ -1,17 +1,17 @@
-package fhtw.cartridgeScaping;
+package fhtw.cartridgeScaping.util;
 
 /**
  * INFO Header of View.java
  *
  * @author Nikolaus Rieder (c)2022
  * @type Enum
- * @path src/main/java/fhtw/cartridgeScaping
+ * @path src/main/java/fhtw/cartridgeScaping/util
  * @project CartridgeScaping
  */
 public enum View {
     MAIN("mainMenu.fxml", true),
     CONFIGURATION("playConfiguration.fxml", true),
-    SETTINGS("applicationSettings.fxml", true),
+    SETTINGS("appSettings.fxml", true),
     WAITING("waitingRoom.fxml", false),
     GAMEPLAY("gameplayView.fxml", false),
     CREDITS("gameCredits.fxml", true);
